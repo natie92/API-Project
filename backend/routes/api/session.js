ba3b78c9e82651c4c98f8735d1964c54c35d226d
natie92 <83873748+natie92@users.noreply.github.com>
2022-10-10 21:07:17 -0400

    added lines in

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -8,41 +8,41 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
-// router.post('/', async (req, res, next) => {
-//     const { credential, password } = req.body;
-//     const user = await User.login({ credential, password });
+router.post('/', async (req, res, next) => {
+    const { credential, password } = req.body;
+    const user = await User.login({ credential, password });
 
-//     if (!user) {
-//       const err = new Error('Login failed');
-//       err.status = 401;
-//       err.title = 'Login failed';
-//       err.errors = ['The provided credentials were invalid.'];
-//       return next(err);
-//     }
+    if (!user) {
+      const err = new Error('Login failed');
+      err.status = 401;
+      err.title = 'Login failed';
+      err.errors = ['The provided credentials were invalid.'];
+      return next(err);
+    }
 
-//     await setTokenCookie(res, user);
+    await setTokenCookie(res, user);
 
-//     return res.json({
-//       user
-//     });
-//   }
-// );
+    return res.json({
+      user
+    });
+  }
+);
 
-// router.delete('/',(_req, res) => {
-//     res.clearCookie('token');
-//     return res.json({ message: 'success' });
-//   }
-// );
+router.delete('/',(_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  }
+);
 
-// router.get('/',restoreUser,(req, res) => {
-//     const { user } = req;
-//     if (user) {
-//       return res.json({
-//         user: user.toSafeObject()
-//       });
-//     } else return res.json({});
-//   }
-// );
+router.get('/',restoreUser,(req, res) => {
+    const { user } = req;
+    if (user) {
+      return res.json({
+        user: user.toSafeObject()
+      });
+    } else return res.json({});
+  }
+);
 
 const validateLogin = [
   check('credential')
