33b9d4495d4baa88b3747f4ec2bec6aa7e7c6871
natie92 <83873748+natie92@users.noreply.github.com>
2022-10-13 17:46:45 -0400

    spot router added

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const spotsRouter = require('./spots.js');
+
 
 // const { setTokenCookie } = require('../../utils/auth.js');
 // const { User } = require('../../db/models');
@@ -38,6 +40,8 @@ router.use(restoreUser);
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+
+router.use('spots', spotsRouter)
 
 
 router.post('/test', (req, res) => {
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,53 +1,53 @@
 const express = require("express");
 
-const { setTokenCookie, restoreUser, requireAuth } = require("../../utils/auth");
+const { restoreUser, requireAuth } = require("../../utils/auth");
 const { Spot, User} = require("../../db/models");
-const sequelize = require("sequelize");
+// const sequelize = require("sequelize");
 
-const { check, query } = require("express-validator");
-const { handleValidationErrors } = require("../../utils/validation");
+// const { check, query } = require("express-validator");
+// const { handleValidationErrors } = require("../../utils/validation");
 
 const router = express.Router();
 
-const validateSpot = [
-  check('address')
-    .exists({ checkFalsy: true })
-    .withMessage('Needs street address'),
-  check('city')
-    .exists({ checkFalsy: true })
-    .withMessage('Needs city'),
-  check('country')
-    .exists({ checkFalsy: true })
-    .withMessage('Needs country'),
-  check('state')
-    .exists({ checkFalsy: true })
-    .withMessage('Needs State'),
-  check('lat')
-    .exists({ checkFalsy: true })
-    .isNumeric({ checkFalsy: true })
-    .withMessage('Invalid Lat'),
-  check('lng')
-    .exists({ checkFalsy: true })
-    .isNumeric({ checkFalsy: true })
-    .withMessage('Invalid Lng'),
-  check("name")
-    .exists({ checkFalsy: true })
-    .isLength({ max: 80 })
-    .withMessage("Invalid name, must be less than 80 characters"),
-  check("description")
-    .exists({ checkFalsy: true })
-    .withMessage("Description must be entered"),
-  check("price")
-    .exists({ checkFalsy: true })
-    .isInt({ checkFalsy: true })
-    .withMessage("Price per night is needed"),
-  handleValidationErrors,
-];
+// const validateSpot = [
+//   check('address')
+//     .exists({ checkFalsy: true })
+//     .withMessage('Needs street address'),
+//   check('city')
+//     .exists({ checkFalsy: true })
+//     .withMessage('Needs city'),
+//   check('country')
+//     .exists({ checkFalsy: true })
+//     .withMessage('Needs country'),
+//   check('state')
+//     .exists({ checkFalsy: true })
+//     .withMessage('Needs State'),
+//   check('lat')
+//     .exists({ checkFalsy: true })
+//     .isNumeric({ checkFalsy: true })
+//     .withMessage('Invalid Lat'),
+//   check('lng')
+//     .exists({ checkFalsy: true })
+//     .isNumeric({ checkFalsy: true })
+//     .withMessage('Invalid Lng'),
+//   check("name")
+//     .exists({ checkFalsy: true })
+//     .isLength({ max: 80 })
+//     .withMessage("Invalid name, must be less than 80 characters"),
+//   check("description")
+//     .exists({ checkFalsy: true })
+//     .withMessage("Description must be entered"),
+//   check("price")
+//     .exists({ checkFalsy: true })
+//     .isInt({ checkFalsy: true })
+//     .withMessage("Price per night is needed"),
+//   handleValidationErrors,
+// ];
 
 // Get all spots
 
-router.get('/spots', async (req, res, next) => {
-    const airbnbspots = await Spot.findAll()
+router.get('/', async (req, res, next) => {
+    let airbnbspots = await Spot.findAll()
 
     let spots = []
 
@@ -57,3 +57,6 @@ router.get('/spots', async (req, res, next) => {
 
     res.json(spots)
 });
+
+
+module.exports = router;
