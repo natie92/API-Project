3d94928b870ac1eea425a92bf27aa63c54b10bdf
natie92 <83873748+natie92@users.noreply.github.com>
2022-10-17 17:57:42 -0400

    added create a review for a spot

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const { Spot, SpotImage, User, Review, ReviewImage } = require("../../db/models");
 const { requireAuth, restoreUser } = require("../../utils/auth.js");
+const sequelize = require("sequelize");
 const router = express.Router();
+
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+
+
+
 
 
 
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -45,6 +45,18 @@ const validateSpot = [
   handleValidationErrors,
 ];
 
+const validateReview = [
+  check("review")
+    .exists({ checkFalsy: true })
+    .withMessage("Review text is required"),
+  check("stars")
+    .exists({ checkFalsy: true })
+    .isInt({ min: 1, max: 5 })
+    .withMessage("Stars must be an integer from 1 to 5"),
+
+  handleValidationErrors,
+];
+
 // Get all spots
 
 router.get('/', async (req, res, next) => {
@@ -305,6 +317,50 @@ router.put('/:spotId', requireAuth, validateSpot, async(req, res, next) => {
         price
     });
     res.json(spot)
+
+});
+
+// create a review for a spot based on the spot's id
+
+router.post('/:spotId/reviews', requireAuth, validateReview, async (req, res, next) => {
+    const { review, stars } = req.body;
+    const spotId = req.params.spotId;
+    const userId = req.user.id;
+    const spot = await Spot.findOne({
+        where: {
+            id: spotId
+        }
+    })
+    if(!spot){
+        return res.status(404).json({
+            message: "Spot couldn't be found",
+            statusCode: 404,
+        })
+    }
+
+    const checkReview = await Review.findOne({
+        where: {
+            userId, spotId
+        },
+        exclude: [
+            { model: Spot, attributes: ['createdAt', 'updatedAt']}
+        ]
+    })
+    if(checkReview){
+        return res.status(403).json({
+            message: 'User already has a review for this spot',
+            statusCode: 403,
+        })
+    }
+
+    const newReview = await Review.create({
+        userId,
+        spotId,
+        review,
+        stars,
+    })
+    res.json(newReview)
+
 
 });
 
