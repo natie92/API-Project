a77fcc25a5f7e1326e528d7aa1598cf9c5e6f310
natie92 <83873748+natie92@users.noreply.github.com>
2022-10-17 21:00:21 -0400

    test

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -136,6 +136,7 @@ router.get('/:spotId', async (req, res, next) => {
                 [ sequelize.fn('AVG', sequelize.col('Reviews.stars')),'avgStarRating'],
             ]
         },
+        group: ['Reviews.review', 'Reviews.stars'],
         include: [
             { model: User, attributes: ['id', 'firstName', 'lastName'] },
             { model: SpotImage, attributes: ['id', 'url','preview']},
