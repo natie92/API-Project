e3cbacc74104237df6f2f524c750d4114c3dd6fe
natie92 <83873748+natie92@users.noreply.github.com>
2022-11-21 19:24:36 -0500

    finished phase 2: signup form

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -40,6 +40,23 @@ export const restoreUser = () => async dispatch => {
   return response;
 };
 
+export const signup = (user) => async (dispatch) => {
+  const { firstName, lastName, username, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      firstName,
+      lastName,
+      username,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data));
+  return response;
+};
+
 const sessionReducer = (state = initialState, action) => {
   let newState;
   switch (action.type) {
